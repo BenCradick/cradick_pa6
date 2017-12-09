06b6ec50b2780865744c27052b5da4c72b0f8e31
Ben Cradick <ben.cradick@gmail.com>
2017-12-08 18:31:28 -0600

    current code

--- runsim.c
+++ runsim.c
@@ -12,10 +12,10 @@
 
 int main(int argc, char* argv[]){
 
-    typedef enum {false, true} bool;
+
     int i = 0;
     //sets pid_t to 0 and gives it a nicer name
-    pid_t pid;
+    pid_t pid = 0;
     //checks appropriate number of arguments are made.
     if(argc != 2)
     {
@@ -40,38 +40,9 @@ int main(int argc, char* argv[]){
     int status;
 
 
-    while(fgets(stream, max_buf, stdin) != NULL)
+    while(!feof(stdin))
     {
-        char streamPart[2][max_buf];
-        int count = 0;
-        bool first = true;
-        for(int i = 0; i < sizeof(stream); i++){
-            if(first && isdigit(stream[i])){
-                first = false;
-                count = 0;
-                while(isdigit(stream[i])){
-                    streamPart[1][count] = stream[i];
-                    printf("%c", streamPart[1][count] );
-                    count++;
-                    i++;
-                }
-                printf("\n");
-                count = 0;
-            }
-            else if(!first && isdigit(stream[i])){
-                while(isdigit(stream[i])){
-                    streamPart[2][count] = stream[i];
-                    printf("%c\n", streamPart[2][count]);
-                    count++;
-                    i++;
-                }
-                printf("\n");
-            }
-            else{
-                streamPart[0][count] = stream[i];
-                count++;
-            }
-        }
+
         if(pr_count == pr_limit){
             wait(&status);
             pr_count--;
@@ -80,16 +51,21 @@ int main(int argc, char* argv[]){
         pid = fork();
         pr_count++;
         if(pid < 0){
-            printf("fork() returned -1 unable to create fork");
+            printf("fork() returned -1 unable to create fork\n");
             break;
         }
-        else if(pid > 0){
-            break;
+        else if(pid == 0){
+            execl(".", "/testsim", "1", "1",(char *)NULL);
         }
-        execl(streamPart[0],streamPart[1], streamPart[2],(char *)NULL);
+        if(waitpid(pid, &status, WNOHANG)!= -1)
+        {
+            pr_count--;
+        }
+        //fprintf(stderr, "pr_count:%d process ID:%d parent ID:%d child ID:%d\n", pr_count, getpid(), getppid(), pid);
+
     }
-
-
+    wait(0);
+    exit(0);
     fprintf(stderr, "pr_count:%d process ID:%d parent ID:%d child ID:%d\n", pr_count, getpid(), getppid(), pid);
-
+    return 0;
 }
